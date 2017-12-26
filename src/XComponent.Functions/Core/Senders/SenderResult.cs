6168dc9caba388bf8b9764a9ccf09ac69fa9d5c0
Yahya NAJAR <yahya.najar@xcomponent.com>
2017-12-26 16:54:13 +0100

    #8 : Changing the type of Sender.SenderParameter

--- src/XComponent.Functions/Core/Senders/SenderResult.cs
+++ src/XComponent.Functions/Core/Senders/SenderResult.cs
@@ -1,9 +1,12 @@
-﻿namespace XComponent.Functions.Core.Senders
+﻿using System;
+
+namespace XComponent.Functions.Core.Senders
 {
+    [Serializable]
     public class SenderResult
     {
         public string SenderName { get; set; }
-        public string SenderParameter { get; set; }
+        public object SenderParameter { get; set; }
         public bool UseContext { get; set; }
     }
 }
